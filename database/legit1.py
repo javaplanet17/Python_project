7550cb06feb3548cef22fe3f6edc13dffe37ecac
java <javaplanet17@gmail.com>
2017-09-01 09:46:43 -0400

    first python file

---
+++ database/legit1.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+#if database does not exist it will automatically create new database
+conn = sqlite3.connect('tutorial.db')
+# cursor execute things
+c = conn.cursor()
+
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)')
+
+def data_entry():
+    c.execute("INSERT INTO stuffToPlot VALUES(145, '2016-01-01', 'python', 5)")
+    conn.commit()
+    c.close()
+    conn.close()
+
+create_table()
+data_entry()
